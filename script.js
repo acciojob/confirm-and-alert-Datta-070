fe8a43f801cbbace65fe186492b0c9a425f7e235
Chinmay Datt D <chinmaydatt003@gmail.com>
2024-01-19 21:40:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+var proceed = confirm("Do you want to proceed?");
+if (proceed) {
+    // If OK is clicked
+    alert("You clicked OK. Proceeding...");
+} else {
+    // If Cancel is clicked
+    alert("You clicked Cancel. Exiting...");
+}
